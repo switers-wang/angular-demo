03168f5a4a7542139ee32045eb3864646b825727
mac <wangxupeng@MacBook-Pro.local>
2018-11-13 00:24:25 +0800

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  template: '<h1>hello world</h1>',
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
