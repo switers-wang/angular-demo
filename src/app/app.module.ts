83d117aadaabcc4934ab8a83641667c08e2be21f
王叙鹏 <wangxupeng@meituan.com>
2018-11-13 14:10:00 +0800

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
+  /** 模块可以包含‘组件’，‘指令’，‘管道’，这里是声明本模块内部成员*/
   declarations: [
     AppComponent
   ],
+  /** 引入其他功能模块 */
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  exports: [AppComponent], /** 将模块内部成员export出去 */
+  bootstrap: [AppComponent] /** 主视图，只有根模块需要*/
 })
 export class AppModule {}
