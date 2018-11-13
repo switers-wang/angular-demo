e893c9176b64fa98e2758be3523dcee1441c363b
王叙鹏 <wangxupeng@meituan.com>
2018-11-13 14:16:11 +0800

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  template: '<h1>hello world</h1>',
+  template: '<h1>hello world</h1><h2>welcome {{name}}</h2>',
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
   title = 'angular-demo';
+  name = 'wangxupeng'
 }
