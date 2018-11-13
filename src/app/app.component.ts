392f4dee4de4903011a6a0e4854aa2af02b7c24e
mac <wangxupeng@MacBook-Pro.local>
2018-11-13 22:56:31 +0800

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,10 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   template: '<h1>hello world</h1>' + 
-    '<h2>welcome {{name}}</h2>' + 
-    '<img [src]="url" />' + 
-    '<button (click)="showName()">click show my name</button>' + 
-    '<input type="text" required [(ngModel)]="inputvalue"><span>输入的值是{{inputvalue}}</span>',
+    '<h2>welcome {{name}}</h2>' +  /** 塞值*/
+    '<img [src]="url" />' +        /** 属性绑定*/
+    '<div><button (click)="showName()">click show my name</button></div>' +   /** 方法绑定*/
+    '<div><input type="text" required [(ngModel)]="inputvalue"><span>输入的值是{{inputvalue}}</span></div>', /** 双向绑定*/
   styleUrls: ['./app.component.less']
 })
 export class AppComponent { 
